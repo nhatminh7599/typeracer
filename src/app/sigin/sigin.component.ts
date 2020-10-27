97a470987a5fbf7d46e023d6d92025332e5df43e
Thanh Luong Van <thanh.lv10041999@gmail.com>
2020-10-27 14:08:28 +0700

    add file

--- src/app/sigin/sigin.component.ts
+++ src/app/sigin/sigin.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ViewChild,ElementRef } from '@angular/core'
+import * as firebase from 'firebase/app'
+import 'firebase/auth'
 
 @Component({
   selector: 'app-sigin',
@@ -8,70 +10,38 @@ import { ViewChild,ElementRef } from '@angular/core'
 })
 export class SiginComponent implements OnInit {
   @ViewChild('loginRef', {static: true }) loginElement: ElementRef;
+  a: any
   auth2: any
   username: any
-  
+  firebaseConfig = {
+    apiKey: "AIzaSyBgvvb0FZif_4CBY6qE_B2mykuixt6q0cE",
+    authDomain: "myprojecttyperace.firebaseapp.com",
+    databaseURL: "https://myprojecttyperace.firebaseio.com",
+    projectId: "myprojecttyperace",
+    storageBucket: "myprojecttyperace.appspot.com",
+    messagingSenderId: "155054231664",
+    appId: "1:155054231664:web:1f2bd565c89093b81364b5",
+    measurementId: "G-HPEYPWL0LZ"
+  };
   constructor() { }
 
   ngOnInit(): void {
-    this.username = ''
-    this.googleInitialize();
-  }
-
-  googleInitialize() {
-    window['googleSDKLoaded'] = () => {
-      window['gapi'].load('auth2', () => {
-        this.auth2 = window['gapi'].auth2.init({
-          client_id: '1047389231963-flfr4hcsjspuqvtmq2th292s9vef4l5q.apps.googleusercontent.com',
-          cookie_policy: 'single_host_origin',
-          scope: 'profile email'
-        });
-        this.prepareLogin();
-      });
-    }
-    (function(d, s, id){
-      var js, fjs = d.getElementsByTagName(s)[0];
-      if (d.getElementById(id)) {return;}
-      js = d.createElement(s); js.id = id;
-      js.src = "https://apis.google.com/js/platform.js?onload=googleSDKLoaded";
-      fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'google-jssdk'));
-  }
-
-  prepareLogin() {
-    this.auth2.attachClickHandler(this.loginElement.nativeElement, {},
-      (googleUser) => {
-        let profile = googleUser.getBasicProfile();
-        console.log('Token || ' + googleUser.getAuthResponse().id_token);
-        console.log('Image URL: ' + profile.getImageUrl());
-        console.log('Email: ' + profile.getEmail());
-        this.username = profile.getEmail()
-      }, (error) => {
-        alert(JSON.stringify(error, undefined, 2));
-      });
   }
   googleLogIn(){
-    // var provider = new firebase.auth.GoogleAuthProvider();
-    // provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
-    // firebase.auth().languageCode = 'pt';
-    // // To apply the default browser preference instead of explicitly setting it.
-    // // firebase.auth().useDeviceLanguage();
-    // firebase.auth().signInWithPopup(provider).then(function(result) {
-    //   // This gives you a Google Access Token. You can use it to access the Google API.
-    //   var token = result.credential.accessToken;
-    //   // The signed-in user info.
-    //   var user = result.user;
-    //   // ...
-    // }).catch(function(error) {
-    //   // Handle Errors here.
-    //   var errorCode = error.code;
-    //   var errorMessage = error.message;
-    //   // The email of the user's account used.
-    //   var email = error.email;
-    //   // The firebase.auth.AuthCredential type that was used.
-    //   var credential = error.credential;
-    //   // ...
-    // });
+    firebase.initializeApp(this.firebaseConfig)
+    var provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope('https://www.googleapis.com/auth/contacts.readonly');
+    firebase.auth().languageCode = 'pt';
+    // To apply the default browser preference instead of explicitly setting it.
+    // firebase.auth().useDeviceLanguage();
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+      console.log("Succes")
+      console.log(result)
+      this.a = result.credential
+      console.log(this.a.accessToken)
+    }).catch(function(error) {
+      console.log("Fail")
+    });
     
   }
 }
--- src/app/typeracer/typeracer.component.ts
+++ src/app/typeracer/typeracer.component.ts
@@ -34,6 +34,7 @@ export class TyperacerComponent implements OnInit {
       this.styleExpression = `width:${this.percent}%`
       this.socketService.setupSocketConnection()
       
+      
     })
     
 
